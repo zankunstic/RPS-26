7338774fb36ac586424b4cc140ae100ea593d36f
zankunstic <zan.kunstic@sts.sckr.si>
2026-09-03 09:45:13 +0000

    if zanka

--- VAJE/uvod.py
+++ VAJE/uvod.py
@@ -1,1 +1,9 @@
-print("hello 1.Ri ")
+
+def hello():
+    x=input("Vnesi svoj razred: ")
+    if x.lower() == "1.ri":
+        print(f"hello {x} ♥")
+    else:
+        print(f"helo {x}")
+if __name__ == "__main__":
+    hello()
